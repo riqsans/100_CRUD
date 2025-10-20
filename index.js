52f2894fd7697e388bd1e4551bda2e04e9bd3a32
riqsans <thrq88@gmail.com>
2025-10-20 10:30:55 +0700

    memperbaiki error

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-let mysql = require('mysql2');
-
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -32,6 +30,7 @@ db.connect((err) => {
 
 //buat method GET dan POST
 
+//GET
 app.get('/api/users', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
         if (err) {
@@ -42,3 +41,56 @@ app.get('/api/users', (req, res) => {
         res.json(results);
     });
 });
+
+
+app.post('/api/users', (req, res) => {
+        const { nama, nim, kelas } = req.body;
+
+    if (!nama || !nim || !kelas) {
+                return res.status(400).json({message: 'nama, nim, kelas wajib diisi'});
+    }
+
+    db.query('INSERT INTO mahasiswa (nama, nim, kelas) VALUES (?, ?, ?)',
+        [nama, nim, kelas], 
+        (err, results) => {
+            if (err) {
+                console.error( err);
+                return res.status(500).json({ message: 'Database Error' });
+            }
+            res.status(201).json({ message: 'Mahasiswa berhasil ditambahkan', id: results.insertId });
+
+        }
+    );
+
+});
+
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { nama, nim, kelas } = req.body;
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ? WHERE id = ?',
+        [nama, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error( err);
+                return res.status(500).json({ message: 'Database Error' });
+            }
+            res.json({ message: 'Mahasiswa berhasil diupdate' });
+        }
+    );
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query(
+        'DELETE FROM mahasiswa WHERE id = ?',
+        [userId],
+        (err, results) => {
+            if (err) {
+                console.error( err);
+                return res.status(500).json({ message: 'Database Error' });
+            }
+            res.json({ message: 'Mahasiswa berhasil dihapus' });
+    });
+});    
+    
